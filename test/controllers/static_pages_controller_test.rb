c22d240f0067c8970c467a87b22f9aefded8cfcd
tsukaryu <drgon8otsuka@gmail.com>
2020-09-16 14:56:35 +0000

    Complete advanced testing setup

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -27,7 +27,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     test "should get contact" do
     get static_pages_contact_url
     assert_response :success
-    assert_select "title", "contact | #{@base_title}"
+    assert_select "title", "Contact | #{@base_title}"
   end
   
 end
